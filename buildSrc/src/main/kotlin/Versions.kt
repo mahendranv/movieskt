cd8db97a70aba8486cd072218c3ae24b12ba4f11
Mahendran <mahendran.vadivalagan@gmail.com>
2023-02-12 18:12:54 +0530

    Lib version updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,9 +4,8 @@ object Versions {
 
     const val navVersion = "2.5.3"
     const val lifecycleVersion = "2.5.1"
-    const val apollo = "2.5.9"
-    const val hilt = "2.44.2"
-    const val coroutines = "1.5.1"
+    const val hilt = "2.45"
+    const val coroutines = "1.6.4"
 
     const val tmdb_Api = "1.12"
     const val coil = "2.2.2"
